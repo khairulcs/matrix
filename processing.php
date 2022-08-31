5faffd71994c534af0c75be4f91d4f0b7373e332
Khairul <khairul.ariffin@feets.me>
2022-08-31 23:23:32 +0800

    update fixes

--- processing.php
+++ processing.php
@@ -1,5 +1,5 @@
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Name submitted</title>
+<title>SMEC - Processing</title>
 <script>
 var tileSize = 80;
 var fadeFactor = 1.4;
